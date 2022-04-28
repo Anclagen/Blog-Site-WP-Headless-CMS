ebf77c8affd5679daab649076b6637b215578203
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-04-28 15:24:27 +0200

    little bit of fiddling

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+const url = "https://fluffypiranha.one/flowerpower/wp-json/wp/v2/posts?_embeded"
+const main = document.querySelector(".main-content");
+
+async function callAPI (url){
+  const response = await fetch(url);
+  const data = await response.json();
+  let content = data[0].content.rendered
+  console.log(data);
+  main.innerHTML = content;
+}
+
+
+
+
+callAPI (url)

48afdfef725552ee0028bf8bffd483a4309f5698
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-05-26 03:00:36 +0200

    refined slider code further

--- js/index.js
+++ js/index.js
@@ -10,15 +10,8 @@ const popularContainer = document.querySelector(".most-commented");
 const bannerImageContainer = document.querySelector(".index-heading");
 const additionalContentContainer = document.querySelector(".additional-content");
 
-// variables for next and previous button functions of latest images slider.
-let latestPageCurrent = 1;
-let latestPageMax = 20;
-let latestPageCurrentMobile = 1;
-let latestPageCurrentTablet = 1;
-let latestPageCurrentDesktop = 1;
-let latestPageMaxMobile = 20;
-let latestPageMaxTablet = 10;
-let latestPageMaxDesktop = 5;
+// variables for slider functionality.
+let sliderLengthMax = 20;
 let latestPostsData = [];
 
 async function createPageContent(){
@@ -30,26 +23,19 @@ async function createPageContent(){
 
     //grabs post data
     latestPostsData = await callAPI(latestPostsUrl);
-    //adjusting page max if I don't add more than 20 posts
-    if(latestPostsData.length < 20){
-    latestPageMaxMobile = latestPostsData.length;
-    latestPageMaxTablet = Math.ceil(latestPostsData.length/2);
-    latestPageMaxDesktop = Math.ceil(latestPostsData.length/4);
-    }
-
+    //latestPostsData = latestPostsData.concat(latestPostsData)
+    
     //adds page content for slider and new
-    createPostImageSlider(latestPostsData, latestContainer);
+    createSliderContent(latestPostsData, latestContainer);
     // resize slider listener
     window.addEventListener("resize", adjustSliderWidths);
 
-    // 2 newest posts
     createPosts(latestPostsData , newestContainer, 2);
 
     //filtering results for commented posts and sorting by most commented
     const commentedPosts = latestPostsData.filter(filterCommentedPosts);
     const sortedCommentedPosts = sortMostCommented(commentedPosts);
 
-    //adds page content for popular
     createPosts(sortedCommentedPosts, popularContainer, 4);
   } catch(error){
     console.log(error)
@@ -86,141 +72,89 @@ function sortMostCommented(data){
 
 /*-------------- Responsive Latest content slider -----------------*/
 
-/* designed to handle the 20 results the call was limited (could be expanded to more)
-   and shrink on mobile to 2 posts at a time from 4*/
+/* Limited to 20 posts max, resizes from 1 to 2 to 4 posts at a time 
+   depending on the screen size, and*/
 
+//
 let slidePercentage = 5;
+let transformMax = 95;
+let transform = 0;
+
+//create slider content
+function createSliderContent(data, container){
+  container.innerHTML= "";
+  sliderLengthMax = data.length;
+  disableButtons();
+  for(let i = 0; i < data.length; i++){
+    if(i === 20){
+      sliderLengthMax = 20
+      break
+    }
+    container.innerHTML += `<div class="latest-slider-content">${createPostCompressed(data[i])}</div>`;
+  }
+}
 
 //function for resize listener to update slider on window resize.
 function adjustSliderWidths(){
-  getWidths();
-  changePageNumber(0);
-  disableButtons();
+  calculateTransform(0);
   transformSlider();
-}
-
-// updates max number of pages and slide % amount
-function getWidths(){
-  if(window.innerWidth < 720){
-    slidePercentage = 5;
-  }
-  else if(window.innerWidth >= 1100){
-    slidePercentage = 20;
-  }else if(window.innerWidth >= 720){
-    slidePercentage = 10;
-  };
+  disableButtons();
 }
 
 // calculates the amount of transform depending on screen width
 function transformSlider(){
-    let transform = (latestPageCurrent - 1) * slidePercentage;
+    //transform = (latestPageCurrent - 1) * slidePercentage;
+      if(window.innerWidth >= 1100){
+        if(transform > ((sliderLengthMax-4) * slidePercentage)){
+          transform = (sliderLengthMax-4) * slidePercentage;
+        }
+      }else if(window.innerWidth >= 720){
+        if(transform > ((sliderLengthMax-2) * slidePercentage)){
+          transform = (sliderLengthMax-2) * slidePercentage;
+      };
+      }
     latestContainer.style.transform = `translateX(-${transform}%)`;
-    console.log(transform)
 }
 
 /*  function to update the page number 
 throw in 0 just for resize, 1 for next, -1 for previous page changes*/
-function changePageNumber(Num){
-  //calculates page number depending on screen size
+function calculateTransform(num){
   if(window.innerWidth < 720){
-    latestPageCurrentMobile = latestPageCurrentMobile + (Num * 1);
-    latestPageCurrentTablet = latestPageCurrentTablet + (Num * 0.5);
-    latestPageCurrentDesktop = latestPageCurrentDesktop + (Num * 0.25);
-    latestPageCurrent = latestPageCurrentMobile;
+    transform += (5*num)
   }else if(window.innerWidth >= 1100){
-    latestPageCurrentMobile = latestPageCurrentMobile + (Num * 4);
-    latestPageCurrentTablet = latestPageCurrentTablet + (Num * 2);
-    latestPageCurrentDesktop = latestPageCurrentDesktop + (Num * 1);
-    latestPageCurrent  = Math.floor(latestPageCurrentDesktop);
+    transform += (20*num)
+  }else if(window.innerWidth >= 720){
+    transform += (10*num)
+  };
+  if(transform < 0){
+    transform = 0;
   }
-  else if(window.innerWidth >= 720){
-    latestPageCurrentMobile = latestPageCurrentMobile + (Num * 2);
-    latestPageCurrentTablet = latestPageCurrentTablet + (Num * 1);
-    latestPageCurrentDesktop = latestPageCurrentDesktop + (Num * 0.5);
-    latestPageCurrent = Math.floor(latestPageCurrentTablet);
-  }
-
-  //correct for the end and start of the slider due to decimals
-  if(latestPageCurrentDesktop < 1){
-    latestPageCurrentDesktop = 1;
-  }
-  if(latestPageCurrentTablet < 1){
-    latestPageCurrentTablet = 1;
-  }
-  if(latestPageCurrentMobile < 1){
-    latestPageCurrentMobile = 1;
-  }
-  if(latestPageCurrentMobile > latestPageMaxMobile){
-    latestPageCurrentMobile = latestPageMaxMobile;
-  }
-  if(latestPageCurrentTablet > latestPageMaxTablet){
-    latestPageCurrentTablet = latestPageMaxTablet;
-  }
-  if(latestPageCurrentDesktop > latestPageMaxDesktop){
-    latestPageCurrentDesktop = latestPageMaxDesktop;
-  }
-  console.log(latestPageCurrentDesktop, latestPageCurrentTablet, latestPageCurrentMobile);
 }
 
-// disables buttons 
+// disables buttons and adjust the max transform according to screen size
 function disableButtons(){
-  if(window.innerWidth < 720){
-    latestPageMax =latestPageMaxMobile;
-    latestPageCurrent = latestPageCurrentMobile;
-  }else if(window.innerWidth >= 1100){
-    //rounds down if desktop page is a decimal
-    latestPageMax = latestPageMaxDesktop;
-    latestPageCurrent  = latestPageCurrentDesktop;
-  }
-  else if(window.innerWidth >= 720){
-    latestPageMax = latestPageMaxTablet;
-    latestPageCurrent = latestPageCurrentTablet;
-  }
-
-  if(latestPageCurrent === 1){
+  if(transform === 0){
     latestPrevious.setAttribute('disabled', 'disabled');
     latestPreviousArrow.style.display = "none";
   } else {
     latestPrevious.disabled = false;
     latestPreviousArrow.style.display = "block";
   }
-
-  if(latestPageCurrent === latestPageMax){
+  if(window.innerWidth < 720){
+    transformMax = (sliderLengthMax-1) * slidePercentage;
+  }if(window.innerWidth >= 1100){
+    transformMax = (sliderLengthMax-4) * slidePercentage;
+  }else if(window.innerWidth >= 720){
+    transformMax = (sliderLengthMax-2) * slidePercentage;
+  }
+  if(transform === transformMax){
       latestNext.setAttribute('disabled', 'disabled');
       latestNextArrow.style.display = "none";
   }
-    if(latestPageCurrent < latestPageMax){
+    if(transform  < transformMax){
       latestNext.disabled = false;
       latestNextArrow.style.display = "block";
     }
-} 
-
-
-//create responsive image slider
-function createPostImageSlider(data, container){
-  container.innerHTML= "";
-  getWidths();
-  disableButtons();
-  let slide = document.createElement("div");
-  slide.classList = "latest-slider-content";
-
-  for(let i = 0; i < data.length; i++){
-    if(i === 20){
-      break
-    }
-    //every 4 posts creates new slide
-    if(i!== 0 && i%4 === 0){
-      container.appendChild(slide);
-      slide = document.createElement("div");
-      slide.classList = "latest-slider-content";
-    }
-    //create post and adds to slide
-    let post = createPostCompressed(data[i]);
-    slide.innerHTML+=post;
-    }
-  //appends last slide
-  container.appendChild(slide);
-  
 }
 
 //grabs for arrows and button variables
@@ -238,16 +172,13 @@ latestPreviousArrow.style.display = "none";
 
 //change page functions for latest posts
 function previousPage(){
-  changePageNumber(-1);
-  disableButtons();
+  calculateTransform(-1);
   transformSlider();
+  disableButtons();
 }
 
 function nextPage(){
-  changePageNumber(1);
-  disableButtons();
+  calculateTransform(1);
   transformSlider();
+  disableButtons();
 }
-
-
-
